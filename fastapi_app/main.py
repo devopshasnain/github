89a166e8d6e513343bd1edf5da814f14d2db4a9c
devopshasnain <techtostory@gmail.com>
2025-12-07 07:41:39 +0000

    new

--- fastapi_app/main.py
+++ fastapi_app/main.py
@@ -1,3 +1,8 @@
+"""
+This module defines FastAPI routes for rendering HTML templates
+and processing form input using FastAPI and Jinja2.
+"""
+
 from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
@@ -6,15 +11,19 @@ app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 
+
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
+    """Render the homepage."""
     return templates.TemplateResponse("index.html", {"request": request})
+
 
 @app.post("/process", response_class=HTMLResponse)
 def process(request: Request, username: str = Form(...)):
+    """Process the submitted form and display a message."""
     message = f"Hello {username}, FastAPI ne aapka data receive kar liya!"
-    return templates.TemplateResponse("index.html", {
-        "request": request,
-        "message": message
-    })
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request, "message": message}
+    )
 
