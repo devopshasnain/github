f88a73decaabd4f309239422bb22512dbdbfe930
devopshasnain <techtostory@gmail.com>
2025-12-07 06:54:10 +0000

    newb

--- test.py
+++ test.py
@@ -1,13 +1,1 @@
-print("hello dosto ")
-name = "hello"
-
-print(name)
-
-def name():
-    print(name)
-def name2():
-    prin
-
-    t("hello world new")
-def name():
-    print("hasnian")
+print("hasnain")
